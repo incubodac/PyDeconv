c9d39e836b53686f1d8a86aca9cc5d45fb5b23f9
Damian Care <damianos.care@gmail.com>
2024-10-30 22:10:52 -0300

    working example for gpu

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ events_of_interest = {
 
 model = {
     "model_name": "targMin",
-    "formula": "y ~  1 + ontarget + scrank*mss", 
+    "formula": "y ~  1 + ontarget", 
     "second_formula": "y ~ 1 + saccade_amplitude",
     "tmin": -0.2,
     "tmax": 0.6,
